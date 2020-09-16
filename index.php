64750763dbbe0938992852bae2ea93e2bdf43ed8
Rupa08 <36546815+Rupa08@users.noreply.github.com>
2020-09-16 12:59:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,43 @@
-<?php echo "Hello World" ; ?>
+<!DOCTYPE html>
+<html lang ="en">
+    <head>
+        <title>Login Page</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/css/uikit.min.css" />
+</head>
+<body>
+
+<div class="uk-section uk-container">
+<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+<form class="uk-form-horizontal uk-margin-large">
+
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-horizontal-text">Username</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-horizontal-text" required="required" type="email" placeholder="Usermane">
+        </div>
+    </div>
+	
+	<div class="uk-margin">
+        <label class="uk-form-label" for="form-horizontal-text">Password</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-horizontal-text" required="required" type="password" placeholder="Password">
+        </div>
+    </div>
+	
+	<div class="uk-margin">
+        <button class="uk.button uk.button.default" type="submit">Login</button>
+    </div>
+
+</form>
+</div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/js/uikit-icons.min.js"></script>
+</body>
+</html>
